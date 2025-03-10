ac19a614a6986964685bb44c586de15ff95c7cec
lukaivanovic <lluka.ivanovic@gmail.com>
2025-03-10 11:20:33 +0100

    test

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { c as createExports } from './chunks/entrypoint_B55xhSyU.mjs';
-import { manifest } from './manifest_BTSkmpTW.mjs';
+import { manifest } from './manifest_DaKbbdRB.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -22,7 +22,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "0a335db8-a89c-436a-8fd3-104fe04f3dd3",
+    "middlewareSecret": "d1bf2b7f-9e3e-4a6b-a466-49d61f6ef6fb",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
